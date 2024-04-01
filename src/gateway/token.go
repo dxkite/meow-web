5cf3393deb0089ec4c6c354182ea6cd42ef0e068
dxkite <dxkite@163.com>
2024-04-01 19:28:25 +0800

    feat: 调整token

--- src/gateway/token.go
+++ src/gateway/token.go
@@ -1,33 +1,25 @@
 package gateway
 
 import (
+	"bytes"
 	"encoding/base64"
 	"encoding/binary"
-	"errors"
 )
 
 type Token struct {
+	Id              uint64
 	ExpireAt        uint64
 	RefreshExpireAt uint64
-	Id              uint64
 }
 
 func (t Token) Marshal() []byte {
-	buf := make([]byte, 24)
-	binary.BigEndian.PutUint64(buf, t.Id)
-	binary.BigEndian.PutUint64(buf[8:16], t.ExpireAt)
-	binary.BigEndian.PutUint64(buf[16:], t.RefreshExpireAt)
-	return buf
+	buf := &bytes.Buffer{}
+	binary.Write(buf, binary.BigEndian, t)
+	return buf.Bytes()
 }
 
 func (t *Token) Unmarshal(buf []byte) error {
-	if len(buf) != 24 {
-		return errors.New("error size")
-	}
-	t.Id = binary.BigEndian.Uint64(buf[:8])
-	t.ExpireAt = binary.BigEndian.Uint64(buf[8:16])
-	t.RefreshExpireAt = binary.BigEndian.Uint64(buf[16:])
-	return nil
+	return binary.Read(bytes.NewBuffer(buf), binary.BigEndian, t)
 }
 
 func (t *Token) EncodeToString() string {
