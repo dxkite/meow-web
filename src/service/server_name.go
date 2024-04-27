bc0cd71f02aaa3f4d0525be261c168d192bea247
dxkite <dxkite@163.com>
2024-04-27 20:34:27 +0800

    feat: 支持联动创建

--- src/service/server_name.go
+++ src/service/server_name.go
@@ -9,13 +9,8 @@ import (
 	"dxkite.cn/meownest/src/entity"
 	"dxkite.cn/meownest/src/repository"
 	"dxkite.cn/meownest/src/utils"
+	"gorm.io/gorm"
 )
-
-type CreateServerNameParam struct {
-	Name          string `json:"name" form:"name" binding:"required"`
-	Protocol      string `json:"protocol" form:"protocol" binding:"required"`
-	CertificateId string `json:"certificate_id" form:"certificate_id"`
-}
 
 type GetServerNameParam struct {
 	Id     string   `json:"id" uri:"id" binding:"required"`
@@ -27,27 +22,68 @@ type ServerName interface {
 	Get(ctx context.Context, param *GetServerNameParam) (*dto.ServerName, error)
 }
 
-func NewServerName(r repository.ServerName, rc repository.Certificate) ServerName {
-	return &serverName{r: r, rc: rc}
+func NewServerName(r repository.ServerName, rc repository.Certificate, sc Certificate, db *gorm.DB) ServerName {
+	return &serverName{r: r, rc: rc, sc: sc, db: db}
 }
 
 type serverName struct {
 	r  repository.ServerName
 	rc repository.Certificate
+	sc Certificate
+	db *gorm.DB
 }
 
-func (s *serverName) Create(ctx context.Context, param *CreateServerNameParam) (*dto.ServerName, error) {
-	rst, err := s.r.Create(ctx, &entity.ServerName{
-		Name:          param.Name,
-		Protocol:      param.Protocol,
-		CertificateId: identity.Parse(constant.CertificatePrefix, param.CertificateId),
-	})
-	if err != nil {
-		return nil, err
-	}
+type CreateServerNameParam struct {
+	Name          string                            `json:"name" form:"name" binding:"required"`
+	Protocol      string                            `json:"protocol" form:"protocol" binding:"required"`
+	CertificateId string                            `json:"certificate_id" form:"certificate_id"`
+	Certificate   *CreateServerNameCertificateParam `json:"certificate" form:"certificate"`
+}
 
-	name := dto.NewServerName(rst)
-	return name, nil
+type CreateServerNameCertificateParam struct {
+	Key         string `json:"key" form:"key" binding:"required"`
+	Certificate string `json:"certificate" form:"key" binding:"required"`
+}
+
+// 创建域名
+// 支持联动创建证书
+func (s *serverName) Create(ctx context.Context, param *CreateServerNameParam) (*dto.ServerName, error) {
+	var name *dto.ServerName
+
+	err := s.dataSource(ctx).Transaction(func(tx *gorm.DB) error {
+		ctx := repository.WithDataSource(ctx, tx)
+
+		var certificateId = param.CertificateId
+		var certificate *dto.Certificate
+
+		if param.Certificate != nil {
+			if cert, err := s.sc.Create(ctx, &CreateCertificateParam{
+				Name:        param.Name,
+				Key:         param.Certificate.Key,
+				Certificate: param.Certificate.Certificate,
+			}); err != nil {
+				return err
+			} else {
+				certificateId = cert.Id
+				certificate = cert
+			}
+		}
+
+		entity, err := s.r.Create(ctx, &entity.ServerName{
+			Name:          param.Name,
+			Protocol:      param.Protocol,
+			CertificateId: identity.Parse(constant.CertificatePrefix, certificateId),
+		})
+		if err != nil {
+			return err
+		}
+
+		name = dto.NewServerName(entity)
+		name.Certificate = certificate
+		return nil
+	})
+
+	return name, err
 }
 
 func (s *serverName) Get(ctx context.Context, param *GetServerNameParam) (*dto.ServerName, error) {
@@ -66,4 +102,8 @@ func (s *serverName) Get(ctx context.Context, param *GetServerNameParam) (*dto.S
 	}
 
 	return name, nil
+}
+
+func (r *serverName) dataSource(ctx context.Context) *gorm.DB {
+	return repository.DataSource(ctx, r.db)
 }
