b8134808e4639e983ab6cef602d617f0de6a0e89
dxkite <dxkite@163.com>
2024-05-04 22:40:51 +0800

    feat: 添加注释

--- src/server/agent.go
+++ src/server/agent.go
@@ -15,6 +15,17 @@ func NewAgent(s service.Agent) *Agent {
 	return &Agent{s: s}
 }
 
+// 重载代理服务路由
+//
+// @Summary      重载代理服务路由
+// @Description  重载代理服务路由
+// @Tags         Agent
+// @Accept       json
+// @Produce      json
+// @Success      200
+// @Failure      400  {object} httpserver.HttpError
+// @Failure      500  {object} httpserver.HttpError
+// @Router       /agent/reload [post]
 func (s *Agent) Reload(c *gin.Context) {
 	s.s.LoadRoute(c)
 	c.Status(http.StatusOK)
