eb2503ff14736fc61dedf3e9d0136cf9f9373758
dxkite <dxkite@163.com>
2024-07-21 23:23:51 +0800

    feat: config

--- src/config/config.go
+++ src/config/config.go
@@ -11,9 +11,9 @@ type Config struct {
 	DataPath                string `env:"DATA_PATH" envDefault:"data.db"`
 	SessionName             string `env:"SESSION_NAME" envDefault:"session_id"`
 	SessionCryptoKey        string `env:"SESSION_CRYPTO_KEY" envDefault:"12345678901234567890123456789012"`
-	MonitorInterval         int    `env:"MONITOR_INTERVAL" envDefault:"5s"`
-	MonitorRollInterval     int    `env:"MONITOR_ROLL_INTERVAL" envDefault:"60s"`
-	MonitorRealtimeInterval int    `env:"MONITOR_REALTIME_INTERVAL" envDefault:"360s"`
+	MonitorInterval         int    `env:"MONITOR_INTERVAL" envDefault:"5"`
+	MonitorRollInterval     int    `env:"MONITOR_ROLL_INTERVAL" envDefault:"60"`
+	MonitorRealtimeInterval int    `env:"MONITOR_REALTIME_INTERVAL" envDefault:"360"`
 }
 
 func Get(ctx context.Context) *Config {
